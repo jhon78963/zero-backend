396967f65b82c319ee2a820404ecc252eda3710d
Jhon Livias <jhonlivias3@gmail.com>
2023-08-02 11:24:00 -0500

    update api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,12 +14,7 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['only' => ['logout','home']]);
-    }
-
-    public function index()
-    {
-        return view('auth.login');
+        $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
     public function login(Request $request)
@@ -27,112 +22,77 @@ class AuthController extends Controller
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
-        ],
-        [
-            'email.required'=>'El email es requerido',
-            'email.max'=>'Maximo 20 caracteres para el username del usuario',
-            'email.email'=>'El email debe tener un formato válido',
-            'password.required'=>'La contraseña es requerida',
-            'password.max'=>'Maximo 20 caracteres permitidos',
-            'password.min'=>'Mínimo 8 caracteres permitidos',
-
         ]);
 
         $credentials = $request->only('email', 'password');
 
-        if (!Auth::attempt($credentials)) {
+        $token = Auth::attempt($credentials);
+
+        if (!$token) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Revise sus credenciales de acceso',
+                'message' => 'Unauthorized',
             ], 401);
         }
 
-        $userExists = User::where('email', $request->email)->first();
-
-        Auth::loginUsingId($userExists->id);
+        $user = Auth::user();
 
         return response()->json([
             'status' => 'success',
-            'message' => 'Bienvenido al sistema'
-        ], 200);
+            'data' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
 
-    }
-
-    public function create()
-    {
-        return view('auth.register');
     }
 
     public function register(Request $request){
-
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-            'cpassword' => 'required|string|min:8',
-        ],
-        [
-            'name.required'=>'El nombre es requerido',
-            'name.max'=>'Maximo 20 caracteres para el nombre del usuario',
-            'email.required'=>'El email es requerido',
-            'email.max'=>'Maximo 20 caracteres para el username del usuario',
-            'email.email'=>'El email debe tener un formato válido',
-            'email.unique'=>'El email ya existe en nuestra base de datos',
-            'password.required'=>'La contraseña es requerida',
-            'password.max'=>'Maximo 20 caracteres permitidos',
-            'password.min'=>'Mínimo 8 caracteres permitidos',
-            'cpassword.required'=>'La contraseña para validar es requerida',
-
         ]);
 
-        if($request->password != $request->cpassword){
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Las contraseñas no coinciden, Porfavor verifique!',
-            ], 401);
-        }
-
-        if(empty($request->termino))
-        {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Porfavor aceptar los terminos y condiciones',
-            ], 401);
-        }
-
-        $userCreated = User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        Auth::loginUsingId($userCreated->id);
+        $token = Auth::login($user);
 
         return response()->json([
             'status' => 'success',
-            'message' => 'Usuario creado con existo'
-        ], 200);
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+    public function refresh()
+    {
+        return response()->json([
+            'status' => 'success',
+            'user' => Auth::user(),
+            'authorisation' => [
+                'token' => Auth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
     }
 
     public function logout()
     {
         Auth::logout();
-        Session::flush();
-        return redirect()->route('auth.login');
-        // return response()->json([
-        //     'status' => 'success',
-        //     'message' => 'Successfully logged out',
-        // ]);
-    }
-
-    public function forgotPassword()
-    {
-        return view('auth.forgot-password');
-    }
-
-    public function home()
-    {
-        return view('auth.home');
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
     }
 
     public function recovery(Request $request)
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -19,6 +20,16 @@ class User extends Authenticatable
     ];
 
     public $timestamps = false;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     public function userRoles()
     {
