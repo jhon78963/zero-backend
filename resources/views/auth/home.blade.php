c0fe46fd2780de5ce349c2acfe9f39b2090ae33f
Jhon Livias <jhon.livias@cordova-studios.com>
2023-09-10 12:56:51 -0500

    delete librarry js

--- resources/views/auth/home.blade.php
+++ resources/views/auth/home.blade.php
@@ -227,17 +227,4 @@
 
 @section('js')
     <script src="{{ asset('assets/js/extended-ui-perfect-scrollbar.js') }}"></script>
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
-    <script src="{{ asset('fullcalendar/dist/index.global.js') }}"></script>
-    <script src="{{ asset('fullcalendar/locales/es.js') }}"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth',
-
-            });
-            calendar.render();
-        });
-    </script>
 @endsection
