d2882c0a90ecbb6358c3e3df8353d28b3aacfa56
Jhon Livias <jhonlivias3@gmail.com>
2023-12-14 14:51:39 -0500

    academic period seeder

---
+++ database/seeders/AcademicPeriodSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AcademicPeriod;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AcademicPeriodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $academic_period = new AcademicPeriod();
+        $academic_period->CreatorUserId = 1;
+        $academic_period->IsDeleted = 0;
+        $academic_period->TenantId = 1;
+        $academic_period->name = 'pa-2023';
+        $academic_period->year = '2023';
+        $academic_period->yearName = 'Año de la unidad, la paz y el desarrollo';
+    }
+}
