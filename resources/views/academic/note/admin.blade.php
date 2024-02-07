c67268fe886f182555e7a07360c6ac13fb60e4e1
Jhon Livias <jhonlivias3@gmail.com>
2024-02-07 11:54:35 -0500

    update select en notas

--- resources/views/academic/note/admin.blade.php
+++ resources/views/academic/note/admin.blade.php
@@ -13,7 +13,8 @@
                 <div class="d-flex align-items-center">
                     <select name="classroom_id" id="classroom_id" class="form-control text-center me-1">
                         @foreach ($classrooms as $classroom)
-                            <option value="{{ $classroom->id }}">
+                            <option value="{{ $classroom->id }}"
+                                {{ $classroom->id == $classroomSelected->id ? 'selected' : '' }}>
                                 {{ $classroom->description }}</option>
                         @endforeach
                     </select>
